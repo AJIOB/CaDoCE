54685745250330f0147b0bd200fb326911fe5e6d
AJIOB <aleksandr9809@gmail.com>
2018-09-22 11:58:15 +0300

    Fix error on y1 output

--- lab2/schema_behav.vhd
+++ lab2/schema_behav.vhd
@@ -9,10 +9,14 @@ entity schema_behav is
 end schema_behav ;
 
 architecture main_arch of schema_behav is
+  signal d2: std_logic;
 begin
+
+  d2 <= ((x1) and (not x2) and (x3)) or ((not x1) and (x2) and (not x3)) or ((x2) and (not x4)) or ((not x1) and (not x4));
+  
   -- outs
-  y1 <= x1 or x2 or (not x3);
+  y1 <= (((not x3) and (not d2)) or ((not x1) and  (not x3)) or ((d2) and (not x1) and (not x2)));
   y2 <= x3 and ((not x1) or (not x2) or x4);
-  y3 <= ((x1) and (not x2) and (x3)) or ((not x1) and (x2) and (not x3)) or ((x2) and (not x4)) or ((not x1) and (not x4));
+  y3 <= d2;
   y4 <= '0';
 end main_arch ; -- main_arch
