38e6813a60334220aa22f29fd8aa6d5418bd8c96
AJIOB <aleksandr9809@gmail.com>
2018-10-24 10:09:50 +0300

    ScaTime test generation was added

--- lab4/lab4_schema_test.vhd
+++ lab4/lab4_schema_test.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use STD.textio.all;
+use ieee.std_logic_textio.all;
 
 entity lab4_schema_test is
 end lab4_schema_test ;
@@ -83,7 +85,25 @@ begin
 
         ready <= '1';
         wait for c_period;
+
+        wait;
     end process;
 
     c <= not c after c_half_period;
+
+    file_work : process
+        file file_res : text is out "output_results.txt";
+        variable out_line : line;
+    begin
+        while (ready /= '1') loop
+            wait for 1 ns;
+            -- d r c
+            write(out_line, d, right, 1);
+            write(out_line, r, right, 1);
+            write(out_line, c, right, 1);
+            writeline(file_res, out_line);
+        end loop;
+        
+        wait;
+    end process ;
 end my_arch ; -- my_arch
