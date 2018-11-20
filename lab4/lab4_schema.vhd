69eedec077f2d8fd17535bcedfeb8b8e9a1cccb1
AJIOB <aleksandr9809@gmail.com>
2018-11-20 19:36:26 +0300

    CLK checking was added

--- lab4/lab4_schema.vhd
+++ lab4/lab4_schema.vhd
@@ -12,26 +12,27 @@ architecture main_arch of lab4_schema is
   signal val : std_logic := 'Z';
 
   signal clk_prev_t : time := 0 ns;
+  signal clk_filtered : std_logic := '0';
 
-  constant clk_duration : time := 5 ns;
+  constant clk_duration : time := 4 ns;
 begin
 
   process(c)
-
+  variable c_delta : time;
   begin
     clk_prev_t <= now;
 
-    if (c = '0') then
-      val <= d;
-    elsif (c = '1' and ((now - clk_prev_t) > clk_duration)) then
-      val <= not d;
-    -- elsif (c'event and (c'last_event < 30 ns)) then
-    --   val <= 'X';
+    c_delta := now - clk_prev_t;
+    if ((c_delta < clk_duration) ) and (c_delta > 0 ns) then
+      clk_filtered <= 'X';
+      assert false report "C need at least 4ns wait between switching" severity error;
     else
-      val <= 'X';
+      clk_filtered <= c;
     end if;
-    --TODO:
+
   end process;
+
+  --TODO:
 
   -- outs
   q <= val;
