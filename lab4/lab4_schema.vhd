7059d33dda2f7c598e120024de696c8a90ca876f
AJIOB <aleksandr9809@gmail.com>
2018-11-20 19:14:22 +0300

    Current signal duration detection example was added

--- lab4/lab4_schema.vhd
+++ lab4/lab4_schema.vhd
@@ -11,13 +11,20 @@ end lab4_schema ;
 architecture main_arch of lab4_schema is
   signal val : std_logic := 'Z';
 
-  constant clk_duration : time := 20 ns;
+  signal clk_prev_t : time := 0 ns;
+
+  constant clk_duration : time := 5 ns;
 begin
 
   process(c)
+
   begin
-    if (c'event and (d'stable(clk_duration))) then
+    clk_prev_t <= now;
+
+    if (c = '0') then
       val <= d;
+    elsif (c = '1' and ((now - clk_prev_t) > clk_duration)) then
+      val <= not d;
     -- elsif (c'event and (c'last_event < 30 ns)) then
     --   val <= 'X';
     else
