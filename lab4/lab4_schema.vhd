3fb533f9336b8a591fd5d73e323099f3bc8ac0a9
AJIOB <aleksandr9809@gmail.com>
2018-11-20 18:44:36 +0300

    lab4 stable example was written

--- lab4/lab4_schema.vhd
+++ lab4/lab4_schema.vhd
@@ -9,35 +9,23 @@ entity lab4_schema is
 end lab4_schema ;
 
 architecture main_arch of lab4_schema is
-    component n
-        port (
-          x: in std_logic;
-          y: out std_logic
-        ) ;
-    end component;
-    component no2
-        port (
-          x1, x2: in std_logic;
-          y:      out std_logic
-        ) ;
-    end component;
-    component noa22
-        port (
-          x1, x2, x3, x4: in std_logic;
-          y: out std_logic
-        ) ;
-    end component;
+  signal val : std_logic := 'Z';
 
-    signal x1, x2, x3, x4, x5, x7, x8: std_logic;
+  constant clk_duration : time := 20 ns;
 begin
-    xx1: n port map (x8, x1);
-    xx2: noa22 port map (x3, x1, d, x8, x2);
-    xx3: no2 port map (r, x2, x3);
-    xx4: noa22 port map (x5, x8, x3, x1, x4);
-    xx5: no2 port map (r, x4, x5);
-    xx7: n port map (x4, x7);
-    xx8: n port map (c, x8);
 
-    -- outs
-    q <= x7;
+  process(c)
+  begin
+    if (c'event and (d'stable(clk_duration))) then
+      val <= d;
+    -- elsif (c'event and (c'last_event < 30 ns)) then
+    --   val <= 'X';
+    else
+      val <= 'X';
+    end if;
+    --TODO:
+  end process;
+
+  -- outs
+  q <= val;
 end main_arch ; -- main_arch
