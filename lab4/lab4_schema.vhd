a9549309f4993198492bba0cd35cf14b5a19f9df
AJIOB <aleksandr9809@gmail.com>
2018-11-21 09:46:38 +0300

    t1, t5, t2, t6 timing checking was implemented. t13 & t14 timings were implemeted with modification

--- lab4/lab4_schema.vhd
+++ lab4/lab4_schema.vhd
@@ -13,9 +13,11 @@ architecture main_arch of lab4_schema is
 
   signal d_prev_t : time := 0 ns;
   signal d_filtered : std_logic := '0';
+  signal d_filtered_after_c : std_logic := '0';
+  signal d_out_now : std_logic := '0';
 
   signal clk_prev_t : time := 0 ns;
-  signal clk_filtered : std_logic := '0';
+  signal c_filtered : std_logic := '0';
 
   signal r_prev_t : time := 0 ns;
   signal r_filtered : std_logic := '0';
@@ -33,10 +35,10 @@ begin
 
     c_delta := now - clk_prev_t;
     if ((c_delta < clk_duration) and (c_delta > 0 ns)) then
-      clk_filtered <= 'X';
+      c_filtered <= 'X';
       assert false report "C need at least 4ns wait between switching" severity error;
     else
-      clk_filtered <= c;
+      c_filtered <= c;
     end if;
   end process;
 
@@ -65,6 +67,42 @@ begin
       assert false report "D need at least 4ns wait between switching" severity error;
     else
       d_filtered <= d;
+    end if;
+  end process;
+
+  process(c_filtered)
+  variable new_d : std_logic;
+  variable d_delay_res : time;
+  begin
+    if (d_filtered = '1') then
+      new_d := '1';
+    elsif (d_filtered = '0') then
+      new_d := '0';
+    end if;
+
+    if (c_filtered = '1') then
+      if (not d_filtered'stable(3 ns)) then
+        -- 0-2 ns
+        d_filtered_after_c <= 'X';
+        assert false report "D need to be stable at least 3ns before C rise" severity error;
+      elsif (not d_filtered'stable(4 ns)) then
+        -- 3 ns
+        d_delay_res := 9 ns;
+      elsif (not d_filtered'stable(5 ns)) then
+        -- 4 ns
+        d_delay_res := 8 ns;
+      elsif (not d_filtered'stable(6 ns)) then
+        -- 5 ns
+        d_delay_res := 7 ns;
+      elsif (d_filtered = '0') then
+        -- 6 ns & more with d = 0
+        d_delay_res := 6 ns;
+      else
+        -- 6 ns & more with d = 1
+        d_delay_res := 7 ns;
+      end if;
+
+      d_out_now <= new_d after d_delay_res;
     end if;
   end process;
 
